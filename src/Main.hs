8fb1a9f5332658ad357306fbcb1fb6676167b295
Juha Paananen <juha.paananen@reaktor.fi>
2012-05-02 15:13:46 +0300

    add host, port params, use ReadWriteMode

--- src/Main.hs
+++ src/Main.hs
@@ -7,23 +7,23 @@ import Text.JSON
 
 main = getArgs >>= huskyBot
 
-huskyBot [name] = do
-  handle <- connectSocket "localhost" 8090
+huskyBot [host, port, name] = do
+  handle <- connectSocket host port
   let json = encode $ toJSObject [("msgType", "join"), ("data", name)]
   hPutStrLn handle json
   hFlush handle
   putStrLn $ ">> " ++ json
   dumpStuff handle
 
-huskyBot _ = putStrLn "USAGE : huskybot <name>"
+huskyBot _ = putStrLn "USAGE : huskybot <host> <port> <name>"
 
 connectSocket host port = do
-  addrinfos <- getAddrInfo Nothing (Just host) (Just (show port))
+  addrinfos <- getAddrInfo Nothing (Just host) (Just port)
   let serveraddr = head addrinfos
   sock <- socket (addrFamily serveraddr) Stream defaultProtocol
   setSocketOption sock KeepAlive 1
   connect sock (addrAddress serveraddr)
-  h <- socketToHandle sock WriteMode
+  h <- socketToHandle sock ReadWriteMode
   hSetBuffering h (BlockBuffering Nothing)
   return h
 
